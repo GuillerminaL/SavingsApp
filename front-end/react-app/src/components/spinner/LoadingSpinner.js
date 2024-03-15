dc8fe8cfe2bb8e68a9cb8f51b16112fabe24f256
Guillermina Lauge <guillerminalauge@gmail.com>
2024-03-15 14:44:18 +0100

    KAN-15 Center loading spinner

--- front-end/react-app/src/components/spinner/LoadingSpinner.js
+++ front-end/react-app/src/components/spinner/LoadingSpinner.js
@@ -2,10 +2,12 @@ import classes from './LoadingSpinner.module.css';
 
 function LoadingSpinner() {
   return (
-    <div className={classes.loadingio}>
+    <div className={classes.grid}>
+      <div className={classes.loadingio}>
       <div className={classes.ldio}>
         <div></div>
       </div>
+    </div>
     </div>
   );
 }
