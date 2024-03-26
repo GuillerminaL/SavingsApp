095c58e5ec965c1138e8a4426f911669671ac005
Guillermina Lauge <guillerminalauge@gmail.com>
2024-03-26 13:07:50 +0100

    KAN-50 Improve Currencies flow (filter by name containing keyword insensitive)

--- back-end/src/routes/currencies.ts
+++ back-end/src/routes/currencies.ts
@@ -5,7 +5,7 @@ import { getCurrencies, getCurrency, addCurrency, patchCurrency, deleteCurrency 
 const router = Router();
 
 router.post('/', addCurrency);
-router.get('/', getCurrencies);    //currencies?name=name                       
+router.get('/', getCurrencies);    //get currencies with name containing keyword /currencies?name=name                       
 router.get('/:currencyId', getCurrency);
 router.patch('/:currencyId', patchCurrency); //ImageUrl
 router.delete('/:currencyId', deleteCurrency);
