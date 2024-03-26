077e353c2833f194dfca09c20fac57efef323229
Guillermina Lauge <guillerminalauge@gmail.com>
2024-03-26 15:58:11 +0100

    KAN-51 Improve Tags flow (deleting checks, filter by name containing keyword insensitive, documentation)

--- back-end/src/controllers/tag.ts
+++ back-end/src/controllers/tag.ts
@@ -5,7 +5,7 @@ import { get500 } from './error';
 import Tag from '../models/tag';
 import Saving from '../models/saving';
 
-type RequestBody = { name: string, description: string | null };
+type RequestBody = { name: string, description: string };
 
 /**
  * Function getTags: 
@@ -60,14 +60,28 @@ export async function getTag(req:Request, res:Response, next: NextFunction) {
     }
 }
 
+/**
+ * Function addTag:
+ * @param req Body: name and description required. Name must be unique
+ * @param res res.status().json{message} | res.status(200).json{message, tag: newTag}
+ * @returns 400 - Must specify tag name and description
+ *          400 - Name must be unique
+ *          500 - Internal error
+ *          201 - New tag created
+ */
 export async function addTag(req:Request, res:Response, next: NextFunction) {
     const body = req.body as RequestBody;
-    const enteredName = body.name;
+    const enteredName = body.name as string;
+    const enteredDescription = body.description as string;
     try {
-        //Checks name input...
+        //Checks inputs...
         if ( ! enteredName ) {
             return res.status(400).json({message: `Must specify a tag name`});
         }
+        if ( ! enteredDescription ) {
+            return res.status(400).json({message: `Must specify a tag description`});
+        }
+        //Check name existence...
         const checkName = await Tag.find({ name: enteredName });
         if ( checkName.length !== 0 ) {
             return res.status(400).json({ 
@@ -77,7 +91,7 @@ export async function addTag(req:Request, res:Response, next: NextFunction) {
         //Saves and returns...
         const tag = new Tag({
             name: enteredName, 
-            description: body.description
+            description: enteredDescription
         });
         const newTag = await tag.save();
         if ( ! newTag ) {
@@ -85,23 +99,34 @@ export async function addTag(req:Request, res:Response, next: NextFunction) {
                 message: 'Something went wrong... We are working hard to solve it!' 
             });
         }
-        return res.status(201).json({ message: 'Added Tag', tag: newTag });        
+        return res.status(201).json({ message: 'Added Tag', newTag: newTag });        
     } catch (error) {
         console.log(error);
         get500(req, res, next);
     }
 }
 
+/**
+ * Function patchTag:
+ *      - Allows to patch name and/or description
+ * @param req Params: tagId, Body: name and/or description (not empty, unique name)
+ * @param res res.status().json{message} | res.status(200).json{message, tag: tag}
+ * @returns 400 - Invalid id
+ *          404 - Not found
+ *          400 - Must specify new name and/or new description
+ *          400 - Existing name
+ *          200 - Updated tag
+ */
 export async function patchTag(req:Request, res:Response, next: NextFunction) {
     const tagId = req.params.tagId;
     try {
         if ( ! isValidObjectId(tagId) ) {
-            return res.status(404).json({message: `Tag id ${tagId} is not a valid id`}); 
+            return res.status(400).json({message: `Tag id ${tagId} is not a valid id`}); 
         }
         //Checks tag existence...
         const toPatchTag = await Tag.findById(tagId);
         if ( ! toPatchTag ) {
-            return res.status(400).json({message: `Tag id ${tagId} does not exist`});
+            return res.status(404).json({message: `Tag id ${tagId} does not exist`});
         }
         //Checks input...
         const enteredName = req.body.name as string | null;
@@ -140,15 +165,24 @@ export async function patchTag(req:Request, res:Response, next: NextFunction) {
     }
 }
 
+/**
+ * Function deleteTag:
+ * @param req tagId
+ * @param res res.status().json{message} | res.status(200).json{message, deletedTag: deletedTag}
+ * @returns 400 - Invalid Id
+ *          409 - Has related savings
+ *          404 - Not found
+ *          200 - Deleted 
+ */
 export async function deleteTag(req:Request, res:Response, next: NextFunction) {
     const tagId = req.params.tagId;
     try {
         if ( ! isValidObjectId(tagId) ) {
-            return res.status(404).json({message: `Tag id ${tagId} is not a valid id`}); 
+            return res.status(400).json({message: `Tag id ${tagId} is not a valid id`}); 
         }
         //Checks related savings existence (active or inactive)...
-        const tagSavings = await Saving.findOne({ tag: tagId });
-        if ( tagSavings ) {
+        const checkTagSavings = await Saving.findOne({ tag: tagId });
+        if ( checkTagSavings ) {
             return res.status(409).json({
                 message: `Can not remove Tag id ${tagId}. It has related savings`
             });
