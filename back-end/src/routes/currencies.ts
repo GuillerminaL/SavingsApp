5491452f24f6284f8addebe7847eb10c2b5e5e0b
Guillermina Lauge <guillerminalauge@gmail.com>
2024-03-26 11:58:25 +0100

    KAN-50 Improves Currency flow (lowercase and deleting checks, filter by name, patch just imageUrl, documentation)

--- back-end/src/routes/currencies.ts
+++ back-end/src/routes/currencies.ts
@@ -7,7 +7,7 @@ const router = Router();
 router.post('/', addCurrency);
 router.get('/', getCurrencies);    //currencies?name=name                       
 router.get('/:currencyId', getCurrency);
-router.patch('/:currencyId', patchCurrency); //Name and imageUrl
+router.patch('/:currencyId', patchCurrency); //ImageUrl
 router.delete('/:currencyId', deleteCurrency);
 
 export default router;
