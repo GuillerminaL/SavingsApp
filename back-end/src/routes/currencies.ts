30a6982a587218136e6de598a548311f68450ec8
Guillermina Lauge <guillerminalauge@gmail.com>
2024-05-08 20:35:03 +0200

    KAN-63 Implements currencies one time population on app init (ruotes update)

--- back-end/src/routes/currencies.ts
+++ back-end/src/routes/currencies.ts
@@ -1,15 +1,12 @@
 import { Router } from "express";
 
-import { getCurrencies, getCurrency, addCurrency, deleteCurrency, getCurrencyCodes, getCurrencyNames } from '../controllers/currency';
+import { getCurrencies, getCurrency, getCurrencyCodes, getCurrencyNames } from '../controllers/currency';
 
 const router = Router();
 
 router.get('/codes', getCurrencyCodes);
 router.get('/names', getCurrencyNames);
-
-router.post('/', addCurrency);
 router.get('/', getCurrencies);    //get currencies with name containing keyword /currencies?name=name                       
 router.get('/:currencyId', getCurrency);
-router.delete('/:currencyId', deleteCurrency);
 
 export default router;
